5962061e6876f9645da2d6115b567781c89697c4
mattbrailsford <me@mattbrailsford.com>
2017-09-19 10:52:02 +0100

    Comment docs for value mapper

--- src/Fluidity/Mappers/FluidityValueMapper.cs
+++ src/Fluidity/Mappers/FluidityValueMapper.cs
@@ -1,9 +1,22 @@
 ﻿namespace Fluidity.Mappers
 {
+    /// <summary>
+    /// Fluidity value mapper
+    /// </summary>
     public abstract class FluidityValueMapper
     {
+        /// <summary>
+        /// Map from Fluidity model to editor model.
+        /// </summary>
+        /// <param name="input">The input.</param>
+        /// <returns>The mapped entity.</returns>
         public abstract object ModelToEditor(object input);
 
+        /// <summary>
+        /// Map from editor model to Fluidity model.
+        /// </summary>
+        /// <param name="input">The input.</param>
+        /// <returns>The mapped entity.</returns>
         public abstract object EditorToModel(object input);
     }
 }
