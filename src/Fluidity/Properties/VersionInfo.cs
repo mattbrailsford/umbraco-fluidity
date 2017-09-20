6094f853e8f3d31381b5f89b92a7dd55fdcbbb29
mattbrailsford <me@mattbrailsford.com>
2017-09-20 14:40:29 +0100

    Updated build script

--- src/Fluidity/Properties/VersionInfo.cs
+++ src/Fluidity/Properties/VersionInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha-000001")]
+[assembly: AssemblyVersion("0.0.*")]
+[assembly: AssemblyInformationalVersion("0.0.0-alpha-000100")]
 
 
