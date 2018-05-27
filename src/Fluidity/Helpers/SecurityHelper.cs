41841bc2733ccdb1d3f8d3f2aac503712ad06160
mattbrailsford <me@mattbrailsford.com>
2018-05-27 14:09:29 +0100

    Implemented security helper

--- src/Fluidity/Helpers/SecurityHelper.cs
+++ src/Fluidity/Helpers/SecurityHelper.cs
@@ -3,18 +3,20 @@
 // Licensed under the Apache License, Version 2.0.
 // </copyright>
 
+using Umbraco.Core;
+
 namespace Fluidity.Helpers
 {
     internal static class SecurityHelper
     {
         internal static string Encrypt(string input)
         {
-            return input;
+            return input.EncryptWithMachineKey();
         }
 
         internal static string Decrypt(string input)
         {
-            return input;
+            return input.DecryptWithMachineKey();
         }
     }
 }
